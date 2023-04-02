e3d98d5c7904a6c48828e4ad510d136bc26c6b73
sjdlr <sjdlrnen0507@naver.com>
2023-04-03 03:29:48 +0900

    feat : [001-EMAIL-Connect] mailgun에 의한 email 연동
    무료 계정으로 테스트시 이메일 등록필요
    vm환경변수로 mailgun-key값 필요

--- src/main/java/com/zerobase/cms/zerobasecms/ZerobaseCmsApplication.java
+++ src/main/java/com/zerobase/cms/zerobasecms/ZerobaseCmsApplication.java
@@ -2,12 +2,18 @@ package com.zerobase.cms.zerobasecms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableFeignClients
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories
 public class ZerobaseCmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZerobaseCmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZerobaseCmsApplication.class, args);
+    }
 
 }
